4446e7b19bbe79563ad37307fa186dc96bfde5bc
AliseBraick <97761594+AliseBraick@users.noreply.github.com>
2022-10-06 14:25:53 -0400

    Add files via upload

---
+++ week04/regex.py
@@ -0,0 +1,23 @@
+# I am not sure if i should include the names in a sentence or a paragraph. I just generate
+# a list of names.
+
+# [A-Z] match a character in the range "A to "Z"
+# [a-z] match a character in the range "a to "z"
+# [-,. '] matches any of these charcters
+# () is to group and + to match all the preceding
+
+import re
+
+
+def find_date(line):
+    pattern = r"[A-Za-z][-,A-Za-z. ']+"
+    result = re.findall(pattern,line)
+    return result
+
+
+f = open("names.txt")
+for line in f.readlines():
+    #print(line)
+    result = find_names(line)
+    if (len(result)>0):
+        print(result)
